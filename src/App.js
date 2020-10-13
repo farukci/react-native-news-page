0ff821b1c729c63f22d0ccfef9e133493bdd92aa
farukci <ferruhcihan20@gmail.com>
2020-10-13 14:31:49 +0300

    project updated

--- src/App.js
+++ src/App.js
@@ -26,54 +26,51 @@ const App = () => {
   }
 
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <View style={styles.body}>
-            <View style={styles.header}>
-              <Image></Image>
-              <Text style={styles.header}>News</Text>
-            </View>
-            {/* {newsBox("'./images/react-native.png'", "News1", "Lorem ipsum dolar sit amet!")} */}
-            <View style={styles.newsboxes}>
-              <Image
-                style={styles.images}
-                source={require('./images/jet-suit.png')}
-              />
-              <Text style={styles.newstitle}>Jet Suit</Text>
-              <Text>Richard Browning starts a business in 2017. He makes jet suits. A jet suit is a special equipment that a...</Text>
-            </View>
-            <View style={styles.newsboxes}>
-              <Image
-                style={styles.images}
-                source={require('./images/insect.png')}
-              />
-              <Text style={styles.newstitle}>One Year ago: Big Insect Problem</Text>
-              <Text>Insects are small animals like ants, bees, bugs or flies. They are very important. Many animals eat them. If there...</Text>
-            </View>
-            <View style={styles.newsboxes}>
-              <Image
-                style={styles.images}
-                source={require('./images/october.jpg')}
-              />
-              <Text style={styles.newstitle}>October Effect</Text>
-              <Text>The October Effect is a perceived market anomaly that stocks tend to fall in October. If we go back in time, back into 1907, a financial crisis ...</Text>
-            </View>
-            <View style={styles.newsboxes}>
-              <Image
-                style={styles.images}
-                source={require('./images/bird.jpg')}
-              />
-              <Text style={styles.newstitle}>Birds of Prey Take Down Drones</Text>
-              <Text>People know that wild hawks may snatch drones in mid-flight, as they mistake them for smaller birds, and it was those incidents which led researchers to assume...</Text>
-            </View>
+    <SafeAreaView>
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={styles.scrollView}>
+        <View style={styles.body}>
+          <View style={styles.header}>
+            <Image></Image>
+            <Text style={styles.header}>News</Text>
           </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+          {/* {newsBox("'./images/react-native.png'", "News1", "Lorem ipsum dolar sit amet!")} */}
+          <View style={styles.newsboxes}>
+            <Image
+              style={styles.images}
+              source={require('./images/jet-suit.png')}
+            />
+            <Text style={styles.newstitle}>Jet Suit</Text>
+            <Text>Richard Browning starts a business in 2017. He makes jet suits. A jet suit is a special equipment that a...</Text>
+          </View>
+          <View style={styles.newsboxes}>
+            <Image
+              style={styles.images}
+              source={require('./images/insect.png')}
+            />
+            <Text style={styles.newstitle}>One Year ago: Big Insect Problem</Text>
+            <Text>Insects are small animals like ants, bees, bugs or flies. They are very important. Many animals eat them. If there...</Text>
+          </View>
+          <View style={styles.newsboxes}>
+            <Image
+              style={styles.images}
+              source={require('./images/october.jpg')}
+            />
+            <Text style={styles.newstitle}>October Effect</Text>
+            <Text>The October Effect is a perceived market anomaly that stocks tend to fall in October. If we go back in time, back into 1907, a financial crisis ...</Text>
+          </View>
+          <View style={styles.newsboxes}>
+            <Image
+              style={styles.images}
+              source={require('./images/bird.jpg')}
+            />
+            <Text style={styles.newstitle}>Birds of Prey Take Down Drones</Text>
+            <Text>People know that wild hawks may snatch drones in mid-flight, as they mistake them for smaller birds, and it was those incidents which led researchers to assume...</Text>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
